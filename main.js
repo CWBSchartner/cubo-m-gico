e83b667e67fd6f17d707f8ddd1388c86017a1aa6
CWBSchartner <163138194+CWBSchartner@users.noreply.github.com>
2024-03-12 09:39:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://www.blog.oncube.com.br/wp-content/uploads/elementor/thumbs/passo1-03-pktjco9tnzaw2ofa2k6aquj4zt5ssm1zfxohao31vo.png";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src = "[https://global.cdn.magazord.com.br/opapapelaria/img/2023/05/produto/3340/cubo-magico-3x3x3-cuber-pro-87517673-7-ae9380048975812628d66133598a4c7b.png?ims=fit-in/600x600/filters:fill(white)";
+
